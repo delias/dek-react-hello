b1d2903003967dd408b517abf3a5543cf5faca94
Daniel Elias <danielek7@gmail.com>
2018-08-30 12:25:36 +1000

    using jsx xample 2

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-ReactDOM.render(React.createElement(
+// Hello world example
+// ReactDOM.render(
+//   <h1>Hello, world3 !</h1>,
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+// example 2
+// const name = 'Daniel Elias';
+// const element = <h2>Hola, {name}</h2>
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+// Example 3
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+var user = {
+  firstName: 'Daniel',
+  lastName: 'Elias'
+};
+
+var element = React.createElement(
   'h1',
   null,
-  'Hello, world2 !'
-), document.getElementById('root'));
+  'Hola, ',
+  formatName(user),
+  '!'
+);
+
+ReactDOM.render(element, document.getElementById('root'));
--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
+// Hello world example
+// ReactDOM.render(
+//   <h1>Hello, world3 !</h1>,
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+// example 2
+// const name = 'Daniel Elias';
+// const element = <h2>Hola, {name}</h2>
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+// Example 3
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Daniel',
+  lastName: 'Elias'
+};
+
+const element = (
+  <h1>
+    Hola, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-  <h1>Hello, world2 !</h1>,
+  element,
   document.getElementById('root')
 );
