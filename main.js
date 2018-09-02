760b13ec488648221b000d7cba0d66abb83faeac
Daniel Elias <danielek7@gmail.com>
2018-09-03 09:25:08 +1000

    State and Lifecycle

--- main.js
+++ main.js
@@ -1,64 +1,82 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function Avatar(props) {
-  return React.createElement("img", {
-    className: "Avatar",
-    src: props.user.avatarUrl,
-    alt: props.user.name
-  });
-}
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-function UserInfo(props) {
-  return React.createElement(
-    "div",
-    { className: "UserInfo" },
-    React.createElement(Avatar, { user: props.user }),
-    React.createElement(
-      "div",
-      { className: "UserInfo-name" },
-      props.user.name
-    )
-  );
-}
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-function Comment(props) {
-  return React.createElement(
-    "div",
-    { className: "Comment" },
-    React.createElement(UserInfo, { user: props.author }),
-    React.createElement(
-      "div",
-      { className: "Comment-text" },
-      props.text
-    ),
-    React.createElement(
-      "div",
-      { className: "Comment-data" },
-      formatDate(props.date)
-    )
-  );
-}
+var Clock = function (_React$Component) {
+  _inherits(Clock, _React$Component);
 
-var comment = {
-  date: new Date(),
-  text: 'Esto es un comentario',
-  author: {
-    name: 'Daniel Elias',
-    avatarUrl: 'src/logoteogracia.png'
+  function Clock(props) {
+    _classCallCheck(this, Clock);
+
+    var _this = _possibleConstructorReturn(this, (Clock.__proto__ || Object.getPrototypeOf(Clock)).call(this, props));
+
+    _this.state = { date: new Date() };
+    return _this;
   }
-};
 
-function App() {
-  return React.createElement(Comment, {
-    date: comment.date,
-    text: comment.text,
-    author: comment.author
-  });
-}
+  _createClass(Clock, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
 
-var element = React.createElement(App, null);
+      this.timerID = setInterval(function () {
+        return _this2.tick();
+      }, 1000);
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  }, {
+    key: 'tick',
+    value: function tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          'Hola, Mundo!'
+        ),
+        React.createElement(
+          'h2',
+          null,
+          'Son las ',
+          this.state.date.toLocaleTimeString(),
+          '.'
+        )
+      );
+    }
+  }]);
 
-ReactDOM.render(element, document.getElementById('root'));
+  return Clock;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Clock, null), document.getElementById('root'));
+
+/**
+ * Let’s quickly recap what’s going on and the order in which the methods are called:
+
+When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock component. Since Clock needs to display the current time, it initializes this.state with an object including the current time. We will later update this state.
+
+React then calls the Clock component’s render() method. This is how React learns what should be displayed on the screen. React then updates the DOM to match the Clock’s render output.
+
+When the Clock output is inserted in the DOM, React calls the componentDidMount() lifecycle hook. Inside it, the Clock component asks the browser to set up a timer to call the component’s tick() method once a second.
+
+Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update by calling setState() with an object containing the current time. Thanks to the setState() call, React knows the state has changed, and calls the render() method again to learn what should be on the screen. This time, this.state.date in the render() method will be different, and so the render output will include the updated time. React updates the DOM accordingly.
+
+If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle hook so the timer is stopped.
+
+ */
--- src/main.js
+++ src/main.js
@@ -1,65 +1,53 @@
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-function Avatar(props) {
-  return (
-    <img 
-      className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
-}
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hola, Mundo!</h1>
+        <h2>Son las {this.state.date.toLocaleTimeString()}.
+        </h2>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-data">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'Esto es un comentario',
-  author: {
-    name: 'Daniel Elias',
-    avatarUrl: 'src/logoteogracia.png',
-  },
-};
-
-function App() {
-  return (
-    <Comment
-      date={comment.date}
-      text={comment.text}
-      author= {comment.author}
-    />
-  );
-}
-
-const element = <App />;
 
 ReactDOM.render(
-  element, 
+  <Clock />,
   document.getElementById('root')
 );
+
+/**
+ * Let’s quickly recap what’s going on and the order in which the methods are called:
+
+When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock component. Since Clock needs to display the current time, it initializes this.state with an object including the current time. We will later update this state.
+
+React then calls the Clock component’s render() method. This is how React learns what should be displayed on the screen. React then updates the DOM to match the Clock’s render output.
+
+When the Clock output is inserted in the DOM, React calls the componentDidMount() lifecycle hook. Inside it, the Clock component asks the browser to set up a timer to call the component’s tick() method once a second.
+
+Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update by calling setState() with an object containing the current time. Thanks to the setState() call, React knows the state has changed, and calls the render() method again to learn what should be on the screen. This time, this.state.date in the render() method will be different, and so the render output will include the updated time. React updates the DOM accordingly.
+
+If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle hook so the timer is stopped.
+
+ */
