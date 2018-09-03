f9c8996e0adc4eb838c3bf3711e8e47211e25ee9
Daniel Elias <danielek7@gmail.com>
2018-09-03 10:41:19 +1000

    Handling Events

--- main.js
+++ main.js
@@ -6,91 +6,41 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-function FormattedDate(props) {
-  return React.createElement(
-    'h2',
-    null,
-    'Son las: ',
-    props.date.toLocaleTimeString(),
-    '.'
-  );
-}
+var Toggle = function (_React$Component) {
+  _inherits(Toggle, _React$Component);
 
-var Clock = function (_React$Component) {
-  _inherits(Clock, _React$Component);
+  function Toggle(props) {
+    _classCallCheck(this, Toggle);
 
-  function Clock(props) {
-    _classCallCheck(this, Clock);
+    var _this = _possibleConstructorReturn(this, (Toggle.__proto__ || Object.getPrototypeOf(Toggle)).call(this, props));
 
-    var _this = _possibleConstructorReturn(this, (Clock.__proto__ || Object.getPrototypeOf(Clock)).call(this, props));
-
-    _this.state = { date: new Date() };
+    _this.state = { isToggleOn: true };
+    //This binding is necessary to make this worl in the callback
+    _this.handleClick = _this.handleClick.bind(_this);
     return _this;
   }
 
-  _createClass(Clock, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      var _this2 = this;
-
-      this.timerID = setInterval(function () {
-        return _this2.tick();
-      }, 1000);
-    }
-  }, {
-    key: 'componentWillUnmount',
-    value: function componentWillUnmount() {
-      clearInterval(this.timerID);
-    }
-  }, {
-    key: 'tick',
-    value: function tick() {
-      this.setState({
-        date: new Date()
+  _createClass(Toggle, [{
+    key: 'handleClick',
+    value: function handleClick() {
+      this.setState(function (prevState) {
+        return {
+          isToggleOn: !prevState.isToggleOn
+        };
       });
     }
   }, {
     key: 'render',
     value: function render() {
       return React.createElement(
-        'div',
-        null,
-        React.createElement(
-          'h1',
-          null,
-          'Hola, Mundo!'
-        ),
-        React.createElement(FormattedDate, { date: this.state.date })
+        'button',
+        { onClick: this.handleClick },
+        this.state.isToggleOn ? 'ON' : 'OFF'
       );
     }
   }]);
 
-  return Clock;
+  return Toggle;
 }(React.Component);
 
-function App() {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(Clock, null),
-    React.createElement(Clock, null),
-    React.createElement(Clock, null)
-  );
-}
-
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
-
-/**
- * Let’s quickly recap what’s going on and the order in which the methods are called:
-
-When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock component. Since Clock needs to display the current time, it initializes this.state with an object including the current time. We will later update this state.
-
-React then calls the Clock component’s render() method. This is how React learns what should be displayed on the screen. React then updates the DOM to match the Clock’s render output.
-
-When the Clock output is inserted in the DOM, React calls the componentDidMount() lifecycle hook. Inside it, the Clock component asks the browser to set up a timer to call the component’s tick() method once a second.
-
-Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update by calling setState() with an object containing the current time. Thanks to the setState() call, React knows the state has changed, and calls the render() method again to learn what should be on the screen. This time, this.state.date in the render() method will be different, and so the render output will include the updated time. React updates the DOM accordingly.
-
-If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle hook so the timer is stopped.
-
- */
+ReactDOM.render(React.createElement(Toggle, null), document.getElementById('root'));
