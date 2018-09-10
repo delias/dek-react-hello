1adf716605da9fe8cacf0d0835f6d928be38d8fb
Daniel Elias <danielek7@gmail.com>
2018-09-10 17:17:34 +1000

    Element Variables

--- src/main.js
+++ src/main.js
@@ -64,8 +64,66 @@ function Greeting(props) {
   }
   else return <GuestGreeting />
 }
+/*
+ReactDOM.render(
+  <Greeting isLoggedIn={true} />,
+  // <Greeting isLoggedIn={false} />,
+  document.getElementById('root')
+);
+*/
+
+// Element Variables
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return(
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <Greeting isLoggedIn={false} />,
+  <LoginControl />,
   document.getElementById('root')
 );
